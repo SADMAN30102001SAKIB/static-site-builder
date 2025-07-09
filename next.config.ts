828aaa78a398680ae8d92fa4bb7a7ba1bfd4a475
SADMAN30102001SAKIB <evan30102001@gmail.com>
2025-07-09 06:10:51 +0600

    updated nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
-  images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
